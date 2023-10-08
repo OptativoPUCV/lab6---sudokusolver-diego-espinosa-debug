849d32410cd5ee83d65388297b69dffd1d701feb
Replit user <>
2023-10-08 08:45:03 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -75,7 +75,7 @@ int is_valid(Node* n){
     for(p=0;p<9;p++){
         int i=3*(k/3) + (p/3) ;
         int j=3*(k%3) + (p%3) ;
-        printf("%d ",nodo->sudo[i][j]);
+        printf("%d ",n->sudo[i][j]);
         if(p%3 == 2) printf("\n");
     }
   
