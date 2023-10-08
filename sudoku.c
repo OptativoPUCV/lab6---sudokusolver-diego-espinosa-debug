911900474d0d0b37f3e9145c19ff347ba60b218c
Replit user <>
2023-10-08 08:44:49 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -70,6 +70,14 @@ int is_valid(Node* n){
       columnas[aux] = 1;
     }
   }
+
+    int k=4,p; 
+    for(p=0;p<9;p++){
+        int i=3*(k/3) + (p/3) ;
+        int j=3*(k%3) + (p%3) ;
+        printf("%d ",nodo->sudo[i][j]);
+        if(p%3 == 2) printf("\n");
+    }
   
   return 1;
 }
