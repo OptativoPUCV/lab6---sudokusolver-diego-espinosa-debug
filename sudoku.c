e70e45776087402e5c040f7b004576514a59c81e
Replit user <>
2023-10-08 08:06:57 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -61,7 +61,7 @@ int is_valid(Node* n){
     {
       int aux = n->sudo[m][t];
       if(columnas[aux] == 1){
-        return 1;
+        return 0;
       }
       columnas[aux] = 1;
     }
@@ -70,7 +70,7 @@ int is_valid(Node* n){
 
   
 
-  return 0;
+  return 1;
 }
 
 
